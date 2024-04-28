b800f218f4b7582d5b88009b2f94e1e1473c6dbe
Alex <graikosal@gmail.com>
2024-04-28 19:48:58 -0400

    updated logos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/histodiff-social-card.png',
       navbar: {
         title: 'My Site',
         logo: {
